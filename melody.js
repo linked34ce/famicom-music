d6273dfd6674d5c602bb12f26f5876c435ff434b
hosoyan <78296405+linked34ce@users.noreply.github.com>
2022-12-22 14:32:28 +0900

    Add files via upload

---
+++ melody.js
@@ -0,0 +1,319 @@
+let melody = [
+
+    { frequency: 1, gain: 0.04, length: 0, delay: 12 },
+
+
+
+    { frequency: 1, gain: 0.04, length: 0, delay: 3 },
+    { frequency: pitches.E5, gain: 0.04, length: 1 / 4, delay: 0 },
+    { frequency: pitches.C5, gain: 0.04, length: 1 / 4, delay: 0 },
+    { frequency: pitches.A4, gain: 0.04, length: 1 / 4, delay: 0 },
+    { frequency: pitches.E4, gain: 0.04, length: 1 / 4, delay: 0 },
+
+    { frequency: pitches.C4, gain: 0.04, length: 4, delay: 0 },
+
+    { frequency: pitches.CS4, gain: 0.04, length: 1 / 8, delay: 0 },
+    { frequency: pitches.DS4, gain: 0.04, length: 1 / 8, delay: 0 },
+    { frequency: pitches.E4, gain: 0.04, length: 13 / 4, delay: 0 },
+    { frequency: pitches.F4, gain: 0.04, length: 1 / 8, delay: 0 },
+    { frequency: pitches.A4, gain: 0.04, length: 1 / 8, delay: 0 },
+    { frequency: pitches.C5, gain: 0.04, length: 1 / 8, delay: 0 },
+    { frequency: pitches.E5, gain: 0.04, length: 1 / 8, delay: 0 },
+
+    { frequency: pitches.A5, gain: 0.04, length: 7, delay: 1 },
+
+
+
+    { frequency: 1, gain: 0.04, length: 0, delay: 4 },
+
+
+
+    { frequency: pitches.GS5, gain: 0.04, length: 1 / 8, delay: 0 },
+    { frequency: pitches.A5, gain: 0.04, length: 13 / 8, delay: 1 / 4 },
+    { frequency: pitches.E5, gain: 0.04, length: 7 / 8, delay: 0 },
+    { frequency: pitches.AS5, gain: 0.04, length: 1 / 8, delay: 0 },
+    { frequency: pitches.B5, gain: 0.04, length: 3 / 4, delay: 1 / 4 },
+
+    { frequency: pitches.A5, gain: 0.04, length: 3 / 2, delay: 0 },
+    { frequency: pitches.AS5, gain: 0.04, length: 1 / 8, delay: 0 },
+    { frequency: pitches.B5, gain: 0.04, length: 11 / 8, delay: 0 },
+    { frequency: pitches.C6, gain: 0.04, length: 3 / 4, delay: 1 / 4 },
+
+    { frequency: pitches.CS6, gain: 0.04, length: 1 / 8, delay: 0 },
+    { frequency: pitches.D6, gain: 0.04, length: 11 / 8, delay: 0 },
+    { frequency: pitches.C6, gain: 0.04, length: 3 / 2, delay: 0 },
+    { frequency: pitches.B5, gain: 0.04, length: 1, delay: 0 },
+
+    { frequency: pitches.A5, gain: 0.04, length: 1, delay: 3 / 8 },
+    { frequency: pitches.DS6, gain: 0.04, length: 1 / 8, delay: 0 },
+    { frequency: pitches.E6, gain: 0.04, length: 3 / 2, delay: 0 },
+    { frequency: pitches.D6, gain: 0.04, length: 1 / 2, delay: 1 / 2 },
+
+    { frequency: pitches.G5, gain: 0.04, length: 3 / 2, delay: 0 },
+    { frequency: pitches.GS5, gain: 0.04, length: 1 / 8, delay: 0 },
+    { frequency: pitches.A5, gain: 0.04, length: 9 / 8, delay: 1 / 4 },
+    { frequency: pitches.AS5, gain: 0.04, length: 1 / 8, delay: 0 },
+    { frequency: pitches.C6, gain: 0.04, length: 7 / 8, delay: 0 },
+
+    { frequency: pitches.B5, gain: 0.04, length: 3 / 2, delay: 0 },
+    { frequency: pitches.A5, gain: 0.04, length: 1, delay: 3 / 8 },
+    { frequency: pitches.AS5, gain: 0.04, length: 1 / 8, delay: 0 },
+    { frequency: pitches.B5, gain: 0.04, length: 1, delay: 0 },
+
+    { frequency: pitches.C6, gain: 0.04, length: 1, delay: 1 / 2 },
+    { frequency: pitches.B5, gain: 0.04, length: 3 / 2, delay: 1 },
+
+    { frequency: pitches.A5, gain: 0.04, length: 3 / 2, delay: 0 },
+    { frequency: pitches.G5, gain: 0.04, length: 1, delay: 0 },
+    { frequency: pitches.GS5, gain: 0.04, length: 1 / 8, delay: 0 },
+    { frequency: pitches.A5, gain: 0.04, length: 7 / 8, delay: 3 / 8 },
+    { frequency: pitches.AS5, gain: 0.04, length: 1 / 8, delay: 0 },
+
+    { frequency: pitches.B5, gain: 0.04, length: 7 / 2, delay: 1 / 2 },
+
+    { frequency: pitches.G5, gain: 0.04, length: 2, delay: 0 },
+    { frequency: pitches.GS5, gain: 0.04, length: 1 / 4, delay: 0 },
+    { frequency: pitches.A5, gain: 0.04, length: 1 / 2, delay: 1 / 4 },
+    { frequency: pitches.E5, gain: 0.04, length: 1, delay: 0 },
+
+    { frequency: pitches.D5, gain: 0.04, length: 5 / 2, delay: 3 / 8 },
+    { frequency: pitches.DS5, gain: 0.04, length: 1 / 8, delay: 0 },
+    { frequency: pitches.E5, gain: 0.04, length: 1, delay: 0 },
+
+    { frequency: pitches.D5, gain: 0.04, length: 3 / 2, delay: 1 / 2 },
+    { frequency: pitches.C5, gain: 0.04, length: 3 / 4, delay: 1 / 4 },
+    { frequency: pitches.D5, gain: 0.04, length: 3 / 4, delay: 1 / 8 },
+    { frequency: pitches.DS5, gain: 0.04, length: 1 / 8, delay: 0 },
+
+    { frequency: pitches.E5, gain: 0.04, length: 7 / 2, delay: 1 / 2 },
+
+    { frequency: pitches.G5, gain: 0.04, length: 3 / 2, delay: 0 },
+    { frequency: pitches.GS5, gain: 0.04, length: 1 / 8, delay: 0 },
+    { frequency: pitches.A5, gain: 0.04, length: 11 / 8, delay: 0 },
+    { frequency: pitches.AS5, gain: 0.04, length: 1 / 8, delay: 0 },
+    { frequency: pitches.B5, gain: 0.04, length: 3 / 8, delay: 1 / 2 },
+
+    { frequency: pitches.C6, gain: 0.04, length: 2, delay: 1 / 4 },
+    { frequency: pitches.CS6, gain: 0.04, length: 1 / 4, delay: 0 },
+    { frequency: pitches.D6, gain: 0.04, length: 5 / 4, delay: 1 / 4 },
+
+    { frequency: pitches.C6, gain: 0.04, length: 9 / 4, delay: 1 / 4 },
+    { frequency: pitches.B5, gain: 0.04, length: 1 / 4, delay: 1 / 4 },
+    { frequency: pitches.G5, gain: 0.04, length: 3 / 4, delay: 1 / 4 },
+
+
+
+    { frequency: pitches.GS5, gain: 0.04, length: 1 / 8, delay: 0 },
+    { frequency: pitches.A5, gain: 0.04, length: 13 / 8, delay: 1 / 4 },
+    { frequency: pitches.E5, gain: 0.04, length: 7 / 8, delay: 0 },
+    { frequency: pitches.AS5, gain: 0.04, length: 1 / 8, delay: 0 },
+    { frequency: pitches.B5, gain: 0.04, length: 3 / 4, delay: 1 / 4 },
+
+    { frequency: pitches.A5, gain: 0.04, length: 3 / 2, delay: 0 },
+    { frequency: pitches.AS5, gain: 0.04, length: 1 / 8, delay: 0 },
+    { frequency: pitches.B5, gain: 0.04, length: 11 / 8, delay: 0 },
+    { frequency: pitches.C6, gain: 0.04, length: 3 / 4, delay: 1 / 4 },
+
+    { frequency: pitches.CS6, gain: 0.04, length: 1 / 8, delay: 0 },
+    { frequency: pitches.D6, gain: 0.04, length: 11 / 8, delay: 0 },
+    { frequency: pitches.C6, gain: 0.04, length: 3 / 2, delay: 0 },
+    { frequency: pitches.B5, gain: 0.04, length: 1, delay: 0 },
+
+    { frequency: pitches.A5, gain: 0.04, length: 1, delay: 3 / 8 },
+    { frequency: pitches.DS6, gain: 0.04, length: 1 / 8, delay: 0 },
+    { frequency: pitches.E6, gain: 0.04, length: 3 / 2, delay: 0 },
+    { frequency: pitches.D6, gain: 0.04, length: 1 / 2, delay: 1 / 2 },
+
+    { frequency: pitches.G5, gain: 0.04, length: 3 / 2, delay: 0 },
+    { frequency: pitches.GS5, gain: 0.04, length: 1 / 8, delay: 0 },
+    { frequency: pitches.A5, gain: 0.04, length: 9 / 8, delay: 1 / 4 },
+    { frequency: pitches.AS5, gain: 0.04, length: 1 / 8, delay: 0 },
+    { frequency: pitches.C6, gain: 0.04, length: 7 / 8, delay: 0 },
+
+    { frequency: pitches.B5, gain: 0.04, length: 3 / 2, delay: 0 },
+    { frequency: pitches.A5, gain: 0.04, length: 1, delay: 3 / 8 },
+    { frequency: pitches.AS5, gain: 0.04, length: 1 / 8, delay: 0 },
+    { frequency: pitches.B5, gain: 0.04, length: 1, delay: 0 },
+
+    { frequency: pitches.C6, gain: 0.04, length: 1, delay: 1 / 2 },
+    { frequency: pitches.B5, gain: 0.04, length: 3 / 2, delay: 1 },
+
+    { frequency: pitches.A5, gain: 0.04, length: 3 / 2, delay: 0 },
+    { frequency: pitches.G5, gain: 0.04, length: 1, delay: 0 },
+    { frequency: pitches.GS5, gain: 0.04, length: 1 / 8, delay: 0 },
+    { frequency: pitches.A5, gain: 0.04, length: 7 / 8, delay: 3 / 8 },
+    { frequency: pitches.AS5, gain: 0.04, length: 1 / 8, delay: 0 },
+
+    { frequency: pitches.B5, gain: 0.04, length: 7 / 2, delay: 1 / 2 },
+
+    { frequency: pitches.G5, gain: 0.04, length: 2, delay: 0 },
+    { frequency: pitches.GS5, gain: 0.04, length: 1 / 4, delay: 0 },
+    { frequency: pitches.A5, gain: 0.04, length: 1 / 2, delay: 1 / 4 },
+    { frequency: pitches.E5, gain: 0.04, length: 1, delay: 0 },
+
+    { frequency: pitches.D5, gain: 0.04, length: 5 / 2, delay: 3 / 8 },
+    { frequency: pitches.DS5, gain: 0.04, length: 1 / 8, delay: 0 },
+    { frequency: pitches.E5, gain: 0.04, length: 1, delay: 0 },
+
+    { frequency: pitches.D5, gain: 0.04, length: 3 / 2, delay: 1 / 2 },
+    { frequency: pitches.C5, gain: 0.04, length: 3 / 4, delay: 1 / 4 },
+    { frequency: pitches.D5, gain: 0.04, length: 3 / 4, delay: 1 / 8 },
+    { frequency: pitches.DS5, gain: 0.04, length: 1 / 8, delay: 0 },
+
+    { frequency: pitches.E5, gain: 0.04, length: 7 / 2, delay: 1 / 2 },
+
+    { frequency: pitches.G5, gain: 0.04, length: 3 / 2, delay: 0 },
+    { frequency: pitches.GS5, gain: 0.04, length: 1 / 8, delay: 0 },
+    { frequency: pitches.A5, gain: 0.04, length: 11 / 8, delay: 0 },
+    { frequency: pitches.AS5, gain: 0.04, length: 1 / 8, delay: 0 },
+    { frequency: pitches.B5, gain: 0.04, length: 3 / 8, delay: 1 / 2 },
+
+    { frequency: pitches.C6, gain: 0.04, length: 2, delay: 1 / 4 },
+    { frequency: pitches.CS6, gain: 0.04, length: 1 / 4, delay: 0 },
+    { frequency: pitches.D6, gain: 0.04, length: 5 / 4, delay: 1 / 4 },
+
+    { frequency: pitches.C6, gain: 0.04, length: 9 / 4, delay: 1 / 4 },
+    { frequency: pitches.B5, gain: 0.04, length: 1 / 4, delay: 1 / 4 },
+    { frequency: pitches.G5, gain: 0.04, length: 3 / 4, delay: 1 / 4 },
+
+
+
+    { frequency: 1, gain: 0.04, length: 0, delay: 1 / 8 },
+
+    { frequency: pitches.B5, gain: 0.04, length: 15 / 2, delay: 1 / 2 },
+
+    { frequency: pitches.A5, gain: 0.04, length: 15 / 2, delay: 1 / 2 },
+
+    { frequency: pitches.G5, gain: 0.04, length: 15 / 2, delay: 1 / 2 },
+
+    { frequency: pitches.E5, gain: 0.04, length: 8, delay: 0 },
+
+    { frequency: 1, gain: 0.04, length: 0, delay: 31 / 8 },
+
+
+
+    { frequency: 1, gain: 0.04, length: 0, delay: 8 },
+
+
+
+    { frequency: 1, gain: 0.04, length: 0, delay: 3 / 2 },
+    { frequency: pitches.E5, gain: 0.04, length: 1 / 2, delay: 1 / 2 },
+    { frequency: pitches.AS5, gain: 0.04, length: 1 / 4, delay: 0 },
+    { frequency: pitches.B5, gain: 0.04, length: 3 / 4, delay: 1 / 2 },
+
+    { frequency: pitches.C6, gain: 0.04, length: 7 / 2, delay: 1 / 2 },
+
+    { frequency: pitches.C6, gain: 0.04, length: 1 / 4, delay: 0 },
+    { frequency: pitches.D6, gain: 0.04, length: 13 / 4, delay: 1 / 2 },
+
+    { frequency: pitches.B5, gain: 0.04, length: 1, delay: 1 / 2 },
+    { frequency: pitches.A5, gain: 0.04, length: 13 / 2, delay: 0 },
+
+    { frequency: pitches.E5, gain: 0.04, length: 15 / 2, delay: 0 },
+    { frequency: pitches.G5, gain: 0.04, length: 1 / 4, delay: 0 },
+    { frequency: pitches.GS5, gain: 0.04, length: 1 / 4, delay: 0 },
+
+    { frequency: pitches.A5, gain: 0.04, length: 8, delay: 0 },
+
+
+
+    { frequency: 1, gain: 0.04, length: 0, delay: 4 },
+
+
+
+    { frequency: pitches.A5, gain: 0.04, length: 3 / 2, delay: 0 },
+    { frequency: pitches.E5, gain: 0.04, length: 3 / 4, delay: 1 / 4 },
+    { frequency: pitches.F5, gain: 0.04, length: 1 / 2, delay: 0 },
+    { frequency: pitches.G5, gain: 0.04, length: 1, delay: 0 },
+
+    { frequency: pitches.B5, gain: 0.04, length: 1 / 8, delay: 0 },
+    { frequency: pitches.C6, gain: 0.04, length: 7 / 8, delay: 0 },
+    { frequency: pitches.G5, gain: 0.04, length: 1 / 2, delay: 0 },
+    { frequency: pitches.F5, gain: 0.04, length: 1, delay: 0 },
+    { frequency: pitches.E5, gain: 0.04, length: 1, delay: 0 },
+    { frequency: pitches.D5, gain: 0.04, length: 1 / 2, delay: 0 },
+
+    { frequency: pitches.DS5, gain: 0.04, length: 1 / 4, delay: 0 },
+    { frequency: pitches.E5, gain: 0.04, length: 7 / 4, delay: 3 / 4 },
+    { frequency: pitches.C5, gain: 0.04, length: 1 / 4, delay: 0 },
+    { frequency: pitches.D5, gain: 0.04, length: 1 / 4, delay: 0 },
+    { frequency: pitches.E5, gain: 0.04, length: 7 / 4, delay: 1 },
+    { frequency: pitches.E5, gain: 0.04, length: 1 / 8, delay: 0 },
+    { frequency: pitches.F5, gain: 0.04, length: 11 / 24, delay: 1 / 6 },
+    { frequency: pitches.E5, gain: 0.04, length: 1 / 2, delay: 1 / 4 },
+    { frequency: pitches.D5, gain: 0.04, length: 1 / 2, delay: 0 },
+
+    { frequency: pitches.DS5, gain: 0.04, length: 1 / 4, delay: 0 },
+    { frequency: pitches.E5, gain: 0.04, length: 11 / 4, delay: 1 },
+
+    { frequency: pitches.D5, gain: 0.04, length: 5 / 4, delay: 1 / 4 },
+    { frequency: pitches.E5, gain: 0.04, length: 1 / 8, delay: 0 },
+    { frequency: pitches.F5, gain: 0.04, length: 11 / 8, delay: 0 },
+    { frequency: pitches.G5, gain: 0.04, length: 1, delay: 0 },
+
+    { frequency: pitches.D5, gain: 0.04, length: 1, delay: 1 / 2 },
+    { frequency: pitches.E5, gain: 0.04, length: 2, delay: 1 / 4 },
+    { frequency: pitches.B4, gain: 0.04, length: 1 / 4, delay: 0 },
+
+    { frequency: pitches.C5, gain: 0.04, length: 3 / 4, delay: 1 / 4 },
+    { frequency: pitches.D5, gain: 0.04, length: 3 / 4, delay: 0 },
+    { frequency: pitches.E5, gain: 0.04, length: 1 / 4, delay: 0 },
+    { frequency: pitches.F5, gain: 0.04, length: 1 / 2, delay: 0 },
+    { frequency: pitches.E5, gain: 0.04, length: 1, delay: 1 / 2 },
+
+
+
+    { frequency: pitches.B4, gain: 0.04, length: 1 / 8, delay: 0 },
+    { frequency: pitches.C5, gain: 0.04, length: 7 / 8, delay: 0 },
+    { frequency: pitches.D5, gain: 0.04, length: 1 / 2, delay: 0 },
+    { frequency: pitches.E5, gain: 0.04, length: 1, delay: 0 },
+    { frequency: pitches.C5, gain: 0.04, length: 1 / 2, delay: 0 },
+    { frequency: pitches.CS5, gain: 0.04, length: 1 / 8, delay: 0 },
+    { frequency: pitches.D5, gain: 0.04, length: 5 / 8, delay: 1 / 4 },
+
+    { frequency: pitches.E5, gain: 0.04, length: 1, delay: 0 },
+    { frequency: pitches.G5, gain: 0.04, length: 1 / 2, delay: 0 },
+    { frequency: pitches.A5, gain: 0.04, length: 1, delay: 0 },
+    { frequency: pitches.B5, gain: 0.04, length: 1, delay: 0 },
+    { frequency: pitches.G5, gain: 0.04, length: 1 / 2, delay: 0 },
+
+    { frequency: pitches.AS5, gain: 0.04, length: 1 / 8, delay: 0 },
+    { frequency: pitches.B5, gain: 0.04, length: 3 / 8, delay: 1 / 4 },
+    { frequency: pitches.B5, gain: 0.04, length: 1 / 2, delay: 1 / 4 },
+    { frequency: pitches.D6, gain: 0.04, length: 1 / 2, delay: 0 },
+    { frequency: pitches.DS6, gain: 0.04, length: 1 / 4, delay: 0 },
+    { frequency: pitches.E6, gain: 0.04, length: 7 / 4, delay: 0 },
+
+    { frequency: 1, gain: 0.04, length: 0, delay: 1 },
+    { frequency: pitches.F6, gain: 0.04, length: 1, delay: 0 },
+    { frequency: pitches.E6, gain: 0.04, length: 1, delay: 0 },
+    { frequency: pitches.D6, gain: 0.04, length: 1, delay: 0 },
+
+    { frequency: pitches.E6, gain: 0.04, length: 3 / 4, delay: 3 / 4 },
+    { frequency: pitches.GS6, gain: 0.04, length: 1 / 4, delay: 0 },
+    { frequency: pitches.A6, gain: 0.04, length: 7 / 4, delay: 1 / 2 },
+
+    { frequency: pitches.D6, gain: 0.04, length: 3 / 2, delay: 0 },
+    { frequency: pitches.C6, gain: 0.04, length: 3 / 2, delay: 0 },
+    { frequency: pitches.B5, gain: 0.04, length: 1, delay: 0 },
+
+    { frequency: pitches.A5, gain: 0.04, length: 7 / 2, delay: 1 / 2 },
+
+    { frequency: pitches.G5, gain: 0.04, length: 1, delay: 1 / 2 },
+    { frequency: pitches.G5, gain: 0.04, length: 3 / 8, delay: 1 / 8 },
+    { frequency: pitches.G5, gain: 0.04, length: 1 / 4, delay: 0 },
+    { frequency: pitches.A5, gain: 0.04, length: 1 / 4, delay: 0 },
+    { frequency: pitches.B5, gain: 0.04, length: 1 / 4, delay: 0 },
+    { frequency: pitches.C6, gain: 0.04, length: 1 / 4, delay: 0 },
+    { frequency: pitches.D6, gain: 0.04, length: 1 / 4, delay: 0 },
+    { frequency: pitches.E6, gain: 0.04, length: 1 / 4, delay: 0 },
+    { frequency: pitches.F6, gain: 0.04, length: 1 / 4, delay: 0 },
+    { frequency: pitches.G6, gain: 0.04, length: 1 / 4, delay: 0 },
+
+    { frequency: pitches.A6, gain: 0.04, length: 4, delay: 0 },
+
+
+
+    { frequency: 1, gain: 0.04, length: 0, delay: 16 }
+
+];
